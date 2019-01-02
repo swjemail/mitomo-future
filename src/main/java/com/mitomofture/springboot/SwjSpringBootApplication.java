47e73ff132b3f8c4806f5f05e3ca97592b1816e6
swjemail <xinghengyan@gmail.com>
2019-01-02 10:50:40 +0900

    hello heroku

---
+++ src/main/java/com/mitomofture/springboot/SwjSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.mitomofture.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwjSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwjSpringBootApplication.class, args);
+	}
+
+}
+
